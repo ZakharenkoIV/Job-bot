90bd6e72ad016a7cbec2d02f29e1f544952796cd
ZakharenkoIV <zaharenkoiv93@gmail.com>
2023-03-08 20:53:38 +0300

    Добавлены начальная реализация класса Telegram-бота и свойства для токена и имени бота

---
+++ src/main/java/ru/example/jobbot/component/TelegramBot.java
@@ -0,0 +1,27 @@
+package ru.example.jobbot.component;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final String botUsername;
+
+    public TelegramBot(@Value("${telegram.bot.token}") String botToken, @Value("${telegram.bot.username}") String botUsername) {
+        super(botToken);
+        this.botUsername = botUsername;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
